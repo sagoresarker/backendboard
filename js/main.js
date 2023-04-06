87985214801bf2ead30a88a8a7c3d4456774fffa
Sagore Sarker <57328825+sagoresarker@users.noreply.github.com>
2023-04-06 23:24:09 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -89,7 +89,7 @@
     * ------------------------------------------------------ */
     var ssFinalCountdown = function() {
 
-        var finalDate =  new Date("April 16, 2023 00:00:00").getTime();
+        var finalDate =  new Date("May 16, 2023 00:00:00").getTime();
         //-date: "Mar 25 2021",
 
         $('.home-content__clock').countdown(finalDate)
